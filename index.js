e76cb79a972da4c3247f52bdba4771b0c2b73588
Austin Holeman <austinholeman@gmail.com>
2022-03-07 18:31:01 -0800

    commit tree c3e01ea1b250b77f77e52f2627df7e48fb5c6c8f

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Add your code here
+function submitData(name, email) {
+    fetch(`http://localhost:3000/users`, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name: 'Steve',
+            email: 'steve@steve.com',
+        })
+    })
+        .then(resp => resp.json)
+        .then(stringify())
+}
